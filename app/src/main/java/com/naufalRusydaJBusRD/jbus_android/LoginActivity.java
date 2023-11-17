549da5c1f21d9c371cc57bed1f27da251db0b187
NaufalRusyda <naufal.rusyda@office.ui.ac.id>
2023-11-17 11:40:33 +0700

    CS07 - Add Intent, MainActivity, Menu

--- app/src/main/java/com/naufalRusydaJBusRD/jbus_android/LoginActivity.java
+++ app/src/main/java/com/naufalRusydaJBusRD/jbus_android/LoginActivity.java
@@ -2,13 +2,47 @@ package com.naufalRusydaJBusRD.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // Define the variables
+    private Button registerNow = null;
+    private Button LoginButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        // Load the components to the variables
+        registerNow = findViewById(R.id.register_button);
+        LoginButton = findViewById(R.id.login_button);
+
+        registerNow.setOnClickListener(v -> {
+            moveActivity(this, RegisterActivity.class);
+            viewToast(this, "Register akun anda");
+        });
+
+        LoginButton.setOnClickListener(v -> {
+            moveActivity(this, MainActivity.class);
+            viewToast(this, "Selamat datang");
+        });
     }
+
+    private void moveActivity(Context ctx, Class<?> cls) {
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String message) {
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
---
+++ app/src/main/java/com/naufalRusydaJBusRD/jbus_android/MainActivity.java
@@ -0,0 +1,25 @@
+package com.naufalRusydaJBusRD.jbus_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+}
